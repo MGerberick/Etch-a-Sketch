60c4c2902b2b969e1bd4f533bd003a33cdf531e2
MGerberick <197294373+MGerberick@users.noreply.github.com>
2025-08-01 11:07:08 -0400

    Day 2. Progress

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+const h = (1/16);
+
 const drawingPad = document.querySelector(".container");
-const drawingRow = document.createElement("div");
 // const drawingCol = document.createElement("div");
+  for (let o = 0; o < 16; o++) { 
+    const drawingRow = document.createElement("div");
+    drawingPad.appendChild(drawingRow);
+    drawingRow.classList.add("row");
+    // drawingRow.textContent = `${o}`;
+    for (let i = 0; i < 16; i++) {
+        let drawingCol = document.createElement("div");
+        drawingRow.appendChild(drawingCol);
+        drawingCol.classList.add("col");
+        // drawingCol.textContent = `${i}`;
+    };
+  };
 
-for (let i = 0; i < 16; i++) {
-    let drawingCol = document.createElement("div");
-    drawingRow.appendChild(drawingCol);
-    drawingCol.textContent = i;
-}
-
-drawingPad.appendChild(drawingRow);
+// drawingPad.appendChild(drawingRow);
 // drawingRow.textContent = 'r';
