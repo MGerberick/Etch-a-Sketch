45f1e5bb13e872f58779bb64d7efd9d419efaf64
MGerberick <197294373+MGerberick@users.noreply.github.com>
2025-08-13 11:21:16 -0400

    Add Grid button

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 const drawingPad = document.querySelector(".container");
-const gridSize = 16;
+let gridSize = 16;
 
-for (let i=0; i<(gridSize**2); i++) {
-  const drawingCell = document.createElement('div');
-  drawingPad.appendChild(drawingCell);
-  drawingCell.classList.add('cell');
-  drawingCell.id=i;
-  let cellWidth = (960/gridSize)+'px';
-  drawingCell.style.width = cellWidth;
+function createPad(){
+  let modrawingCell = document.querySelectorAll(".cell");
+  modrawingCell.forEach(item => item.remove());
+
+  for (let i=0; i<(gridSize**2); i++) {
+    const drawingCell = document.createElement('div');
+    drawingPad.appendChild(drawingCell);
+    drawingCell.classList.add('cell');
+    drawingCell.id=i;
+    let cellWidth = (960/gridSize)+'px';
+    drawingCell.style.width = cellWidth;
+
+    let modrawingCells = document.querySelectorAll(".cell");
+
+    modrawingCells.forEach(item => item.addEventListener("mouseover", () => colorCell(item)));
+  }
 }
-
-const modrawingCells = document.querySelectorAll(".cell");
-
-modrawingCells.forEach(item => item.addEventListener("mouseover", () => colorCell(item)));
 
 function colorCell(item) {
   let element = document.getElementById(item.id);
   element.style.backgroundColor = 'black';
 }
+
+const btnGridSize = document.querySelector(".gridSize");
+
+btnGridSize.addEventListener("click", ()=> createPad());
 
 
