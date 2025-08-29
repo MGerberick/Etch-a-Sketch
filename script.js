bb111693e9fd1a65ce8a1a819d4e8a9f61185323
MGerberick <197294373+MGerberick@users.noreply.github.com>
2025-08-29 14:34:43 -0400

    Add "Clear" button and randomize background color

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const drawingPad = document.querySelector(".container");
 let gridSize = 16;
 
-function createPad(){
-  let modrawingCell = document.querySelectorAll(".cell");
-  modrawingCell.forEach(item => item.remove());
+const btnGridSize = document.querySelector(".gridSize");
+btnGridSize.addEventListener("click", ()=> getUserInput());
 
+const btnGridClear = document.querySelector(".gridClear");
+btnGridClear.addEventListener("click", ()=> clearGrid())
+
+function createPad(){
   for (let i=0; i<(gridSize**2); i++) {
     const drawingCell = document.createElement('div');
     drawingPad.appendChild(drawingCell);
@@ -21,11 +24,38 @@ function createPad(){
 
 function colorCell(item) {
   let element = document.getElementById(item.id);
-  element.style.backgroundColor = 'black';
+
+  if (element.style.backgroundColor == '') {
+    element.style.backgroundColor = generateRandomRgbColor();
+ }
 }
 
-const btnGridSize = document.querySelector(".gridSize");
+function getUserInput() {
+  clearGrid();
+  
+  const userInput = prompt('Please enter a grid size between 1 and 100:', '16')
 
-btnGridSize.addEventListener("click", ()=> createPad());
+  if (userInput == null) {
+    //Do nothing, the user entered "Cancel"
+  } else if (Math.floor(userInput) > 0 && Math.floor(userInput) < 101) {
+    gridSize = userInput;
+    createPad();
+  } else {
+    alert('Please review and try again. Or not. Up to you.')
+  }
+}
 
+function clearGrid() {
+  let modrawingCell = document.querySelectorAll(".cell");
+  modrawingCell.forEach(item => item.remove());
+}
+
+function generateRandomRgbColor() {
+  // Generate random numbers for red, green, and blue components
+  const r = Math.floor(Math.random() * 256); // 0-255
+  const g = Math.floor(Math.random() * 256); // 0-255
+  const b = Math.floor(Math.random() * 256); // 0-255
+
+  return `rgb(${r},${g},${b})`;
+}
 
